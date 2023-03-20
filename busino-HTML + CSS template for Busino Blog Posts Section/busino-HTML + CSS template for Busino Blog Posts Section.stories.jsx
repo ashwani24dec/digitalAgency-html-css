212a45cb3aa7087ead78d5228d3b3f1e307918ab
AshwaniRanchi <nitjsr.ashwani@gmail.com>
2023-03-20 22:30:11 +0530

    ok1

---
+++ busino-HTML + CSS template for Busino Blog Posts Section/busino-HTML + CSS template for Busino Blog Posts Section.stories.jsx
@@ -0,0 +1,11 @@
+import './index';
+const patternDefinition = require('./busino-HTML + CSS template for Busino Blog Posts Section.yml');
+
+export const wingsuit = {
+  patternDefinition,
+  parameters: {
+    docs: {
+      page: null,
+    },
+  },
+};
---
+++ busino-HTML + CSS template for Busino Blog Posts Section/index.js
@@ -0,0 +1,6 @@
+/**
+ * busino-services-icons
+ */
+import './busino-services-icons.twig';
+import './busino-services-icons.wingsuit.yml';
+import './busino-services-icons.css';
